cabed65426881e629136693e6e7b7c6beefd13f9
caarlosdamian <dapecarlos@gmail.com>
2021-07-11 00:49:02 -0500

    add redux add styles, add components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import ProductListing from "./containers/ProductListing";
+import ProductDetail from "./containers/ProductDetail";
+import Header from "./containers/Header";
 
 function App() {
   return (
     <div className="App">
-     <h1>Redux</h1>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" exact component={ProductDetail} />
+          <Route>404 Not Found</Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
